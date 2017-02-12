a9f33a7f342b09838c5eeb7093f272b0f5d21612
brad <myersbrad77@gmail.com>
2017-02-12 17:18:04 -0500

    updated README and List component

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # provider-directory
-
-This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import FontIcon from 'material-ui/FontIcon';
-import IconButton from 'material-ui/IconButton';
-import { map } from 'ramda';
 
 const List = ({ data }) =>
   <div>
