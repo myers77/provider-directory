f84e5f85755d9fa90098bea328956a3c75deb4a0
brad <myersbrad77@gmail.com>
2017-02-15 15:42:31 -0500

    removed more unused code

--- src/index.js
+++ src/index.js
@@ -6,24 +6,20 @@ import createLogger from 'redux-logger';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-import directoryApp from './reducers/index.js';
+import directoryApp from './reducers/index';
 import App from './components/App';
 
 
 // Needed for onTouchTap
 injectTapEventPlugin();
 
-// const initialState = {
-//   attributes,
-// }
-
-// const enhancer = compose(applyMiddleware(createLogger()));
+const enhancer = compose(applyMiddleware(createLogger()));
 // const store = createStore(directoryApp, initialState, enhancer);
-// const store = createStore(directoryApp, enhancer);
-const store = createStore(
-  directoryApp, /* preloadedState, */
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
+const store = createStore(directoryApp, enhancer);
+// const store = createStore(
+  // directoryApp, /* preloadedState, */
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+// );
 
 render(
   <Provider store={store}>
@@ -31,5 +27,5 @@ render(
       <App />
     </MuiThemeProvider>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
